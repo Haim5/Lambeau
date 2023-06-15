b996181b0da2532855a83a3428c96bacac819994
Haim5 <89258839+Haim5@users.noreply.github.com>
2023-06-15 19:26:24 +0300

    Delete ClusteredPerimeterVolumeSorter.java

--- src/ClusteredPerimeterVolumeSorter.java
+++
@@ -1,9 +0,0 @@
-public class ClusteredPerimeterVolumeSorter extends ClusteredSorter {
-    public ClusteredPerimeterVolumeSorter(Bin bin, int lambda) {
-        super(bin.getHeight() + bin.getDepth() + bin.getWidth(), new VolumeSorter(), lambda);
-    }
-    @Override
-    protected int getValue(Package p) {
-        return p.getWidth() + p.getDepth() + p.getHeight();
-    }
-}
